307674ed78446e68e70c23dd47557a58a6893d8d
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-05-25 10:54:16 +0200

    create basic project

---
+++ qml/MainWindow.qml
@@ -0,0 +1,11 @@
+import QtQuick 6.2
+import QtQuick.Controls 6.2
+import QtQuick.Layouts 6.2
+
+ApplicationWindow {
+    id: root
+    visibility: Window.Maximized
+    visible: true
+
+    
+}
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char** argv)
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.addImportPath("qrc:/imports");
+    engine.load(QUrl("qrc:///qml/MainWindow.qml"));
+
+    if (engine.rootObjects().isEmpty())
+    {
+        qDebug() << "Failed to create the main window!";
+        return EXIT_FAILURE;
+    }
+
+    return app.exec();
+}
