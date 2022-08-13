b8b9b934fd53a69b649dc00aad4b663ee9fbe682
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-08-13 16:06:40 +0200

    add selection model class to handle selection

---
+++ include/SelectionModel.h
@@ -0,0 +1,109 @@
+#ifndef SIMPLEAUDIOPLAYER_SELECTIONMODEL_H_
+#define SIMPLEAUDIOPLAYER_SELECTIONMODEL_H_
+
+#include <QAbstractListModel>
+#include <QVariant>
+#include <QList>
+
+/*
+Handle selection.
+Base class for all list base model class.
+*/
+class SelectionModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    SelectionModel(QObject* parent = nullptr);
+    virtual ~SelectionModel();
+
+    enum SelectionRole
+    {
+        IS_SELECTED = Qt::UserRole + 1, // If an element is selected.
+        MAX_ROLE, // The children role will start from this enum value.
+    };
+
+    virtual int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    virtual QHash<int, QByteArray> roleNames() const override;
+
+    /*
+    Clear everything (the data and the selection list).
+    */
+    Q_INVOKABLE void clear();
+
+    /*
+    Clear the selection.
+    */
+    Q_INVOKABLE void clearSelection();
+
+    /*
+    Select an item and clear all other selection.
+    */
+    Q_INVOKABLE void selectAtIndex(int index);
+
+    /*
+    Called when the user selected an item with shift.
+    */
+    Q_INVOKABLE void shiftSelectItem(int index);
+
+    /*
+    Called when the user selected an item with ctrl.
+    */
+    Q_INVOKABLE void ctrlSelectItem(int index);
+
+    /*
+    Return the list of selected indices.
+    */
+    const QList<int>& selectedIndices() const;
+
+    /*
+    Return the items inside the list.
+    */
+    QVariantList itemList() const;
+
+    /*
+    Get an item at a specific index.
+    */
+    QVariant item(int index) const;
+
+    /*
+    Return the selected items inside the list.
+    */
+    QVariantList selectedItemList() const;
+
+    /*
+    Set the items list.
+    */
+    void setItemList(const QVariantList& variantList);
+
+protected:
+    /*
+    Return the lowest index inside m_selectionOrder list.
+    */
+    int lowestSelectedIndex() const;
+
+private:
+    /*
+    Retrieve the items inside the list.
+    onlySelected: if true, return only the selected items, if not, return the entire list.
+    */
+    inline QVariantList _itemList(bool onlySelected) const;
+
+    /*
+    Struct to hold the item selection property and the item itself.
+    */
+    struct ListItem
+    {
+        bool isSelected; // If the element is selected.
+        QVariant item; // Item. A Variant to allow subclass to use whatever element type they want.
+    };
+
+    // Holding the data of the model.
+    QList<ListItem> m_listData;
+
+    // Order of the selection (holding the indices).
+    QList<int> m_selectionOrder;
+};
+
+#endif // SIMPLEAUDIOPLAYER_SELECTIONMODEL_H_
---
+++ src/SelectionModel.cpp
@@ -0,0 +1,235 @@
+#include "SelectionModel.h"
+
+SelectionModel::SelectionModel(QObject* parent) :
+    QAbstractListModel(parent)
+{}
+
+SelectionModel::~SelectionModel()
+{}
+
+int SelectionModel::rowCount(const QModelIndex& parent) const
+{
+    return m_listData.count();
+}
+
+QVariant SelectionModel::data(const QModelIndex& index, int role) const
+{
+    // Check if the index is valid.
+    if (index.isValid() &&
+        index.column() == 0 &&
+        index.row() >= 0 && index.row() < rowCount())
+    {
+        // If the view ask if the item is selected.
+        if (role == IS_SELECTED)
+        {
+            return m_listData.at(index.row()).isSelected;
+        }
+    }
+
+    return QVariant();
+}
+
+QHash<int, QByteArray> SelectionModel::roleNames() const
+{
+    /*
+    Return the roles available for this model has string to 
+    allow call them directly from JavaScript.
+    */
+    QHash<int, QByteArray> roles;
+    roles[IS_SELECTED] = "isSelected";
+    return roles;
+}
+
+int SelectionModel::lowestSelectedIndex() const
+{
+    // Get index position to an invalid value (one value ahead of max index of m_listData).
+    int lowestIndex = m_listData.size();
+        
+    // Getting the lowest index inside m_selectionOrder list.
+    foreach (int index, m_selectionOrder)
+    {
+        if (index < lowestIndex)
+        {
+            lowestIndex = index;
+        }
+    }
+
+    // If the lowest index, return it, otherwise, return -1 (error).
+    if (lowestIndex != m_listData.size())
+        return lowestIndex;
+    else
+        return -1;
+}
+
+void SelectionModel::clear()
+{
+    m_selectionOrder.clear();
+    m_listData.clear();
+}
+
+void SelectionModel::clearSelection()
+{
+    // Clear all the selection.
+    m_selectionOrder.clear();
+    for (ListItem& item : m_listData)
+    {
+        item.isSelected = false;
+    }
+
+    // Notify the view of the modification.
+    emit dataChanged(index(0), index(m_listData.size()-1));
+}
+
+void SelectionModel::selectAtIndex(int index)
+{
+    // Check if the index is valid.
+    if (index >= 0 && index < m_listData.size())
+    {
+        // Clear all the selection.
+        clearSelection();
+
+        // Select the item at index (index).
+        m_listData[index].isSelected = true;
+
+        // Put the index into the selection order list.
+        m_selectionOrder.append(index);
+
+        // Notify the view.
+        emit dataChanged(this->index(index), this->index(index), {IS_SELECTED});
+    }
+}
+
+void SelectionModel::shiftSelectItem(int index)
+{
+    // Check if the index is valid.
+    if (index >= 0 && index < m_listData.size())
+    {
+        // Getting the lowest selected index.
+        int lowestIndex = lowestSelectedIndex();
+
+        // If index lower than lowestIndex. Select from index to lowestIndex.
+        if (index < lowestIndex)
+        {
+            clearSelection();
+            for (int i = index; i <= lowestIndex; i++)
+            {
+                m_listData[i].isSelected = true;
+                m_selectionOrder.append(i);
+            }
+            emit dataChanged(this->index(index), this->index(lowestIndex), {IS_SELECTED});
+        }
+
+        // If index greater than lowestIndex. Select from lowestIndex to index.
+        else if (index > lowestIndex && lowestIndex != -1)
+        {
+            clearSelection();
+            for (int i = lowestIndex; i <= index; i++)
+            {
+                m_listData[i].isSelected = true;
+                m_selectionOrder.append(i);
+            }
+            emit dataChanged(this->index(lowestIndex), this->index(index), {IS_SELECTED});
+        }
+        // Otherwise, this item is already selected or lowestIndex is invalid. Don't do anything.
+    }
+}
+
+void SelectionModel::ctrlSelectItem(int index)
+{
+    // Check if the index is valid.
+    if (index >= 0 && index < m_listData.size())
+    {
+        // Swap isSelected of the item.
+        m_listData[index].isSelected = !m_listData[index].isSelected;
+
+        // Remove (if any) the index of the item from m_selectionOrder list.
+        int listSortingIndex = m_selectionOrder.indexOf(index);
+        if (listSortingIndex != -1)
+        {
+            m_selectionOrder.remove(listSortingIndex);
+        }
+
+        // If the item is selected, add the index of the item at the end of m_selectionOrder list.
+        if (m_listData.at(index).isSelected)
+        {
+            m_selectionOrder.append(index);
+        }
+
+        // Notify the view of the change.
+        emit dataChanged(this->index(index), this->index(index), {IS_SELECTED});
+    }
+}
+
+const QList<int>& SelectionModel::selectedIndices() const
+{
+    return m_selectionOrder;
+}
+
+QVariantList SelectionModel::itemList() const
+{
+    return _itemList(false);
+}
+
+QVariant SelectionModel::item(int index) const
+{
+    // Check if index is valid.
+    if (index >= 0 && index < m_listData.size())
+    {
+        return m_listData.at(index).item;
+    }
+
+    return QVariant();
+}
+
+QVariantList SelectionModel::selectedItemList() const
+{
+    return _itemList(true);
+}
+
+inline QVariantList SelectionModel::_itemList(bool onlySelected) const
+{
+    QVariantList variantList;
+
+    // If onlySelected is false, resize variantList to the size of m_listData.
+    if (!onlySelected)
+    {
+        variantList.resize(m_listData.size());
+        
+        // If onlySelected is false, retrieve all the items inside m_listData.
+        for (int i = 0; i < m_listData.size(); i++)
+        {
+            variantList[i] = m_listData.at(i).item;
+        }
+    }
+
+    // Otherwise, retrieve only the selected items in the right order.
+    else
+    {
+        foreach (int index, m_selectionOrder)
+        {
+            // Check if the index is valid.
+            if (index >= 0 && index < m_listData.size())
+            {
+                // Append the item into the variantList.
+                variantList.append(m_listData.at(index).item);
+            }
+        }
+    }
+
+    return variantList;
+}
+
+void SelectionModel::setItemList(const QVariantList& variantList)
+{
+    // Clear everyting in the list.
+    clear();
+    
+    // Resize m_listData to the size of variantList.
+    m_listData.resize(variantList.size());
+
+    // Copy data from variantList into m_listData.
+    for (int i = 0; i < m_listData.size(); i++)
+    {
+        m_listData[i] = {false, variantList.at(i)};
+    }
+}
