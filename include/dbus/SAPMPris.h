0c78b7141a01f91b4c2eaa1ea6c539ba52525fae
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-10-21 14:50:18 +0200

    dbus: SAPMPris control booth root and player

--- include/dbus/SAPMPris.h
+++ include/dbus/SAPMPris.h
@@ -3,43 +3,124 @@
 
 #include <QObject>
 #include <qtmetamacros.h>
-#include <QDBusPendingReply>
+#include <QDBusObjectPath>
 
 class SAPMPris : public QObject
 {
     Q_OBJECT
-    Q_CLASSINFO("DBus MPRIS interface", "org.mpris.MediaPlayer2")
 
-    Q_PROPERTY(bool CanQuit READ canQuit NOTIFY canQuitChanged)
-    Q_PROPERTY(bool CanRaise READ canRaise NOTIFY canRaiseChanged)
-    Q_PROPERTY(bool HasTrackList READ hasTrackList NOTIFY hasTrackListChanged)
-    Q_PROPERTY(QString Identity READ identity NOTIFY identityChanged)
-    Q_PROPERTY(QStringList SupportedUriSchemes READ supportedUriSchemes NOTIFY supportedUriSchemesChanged)
-    Q_PROPERTY(QStringList SupportedMimeTypes READ supportedMimeTypes NOTIFY supportedMimeTypesChanged)
+    // Root mpris properties
+    Q_PROPERTY(bool CanQuit READ canQuit)
+    Q_PROPERTY(bool CanRaise READ canRaise)
+    Q_PROPERTY(bool HasTrackList READ hasTrackList)
+    Q_PROPERTY(QString Identity READ identity)
+    Q_PROPERTY(QStringList SupportedUriSchemes READ supportedUriSchemes)
+    Q_PROPERTY(QStringList SupportedMimeTypes READ supportedMimeTypes)
+
+    // Player mpris properties
+    Q_PROPERTY(QString PlaybackStatus READ playbackStatus)
+    Q_PROPERTY(double Rate READ rate WRITE setRate)
+    Q_PROPERTY(QVariantMap Metadata READ metadata)
+    Q_PROPERTY(double Volume READ volume WRITE setVolume)
+    Q_PROPERTY(long long Position READ position)
+    Q_PROPERTY(double MinimumRate READ minimumRate)
+    Q_PROPERTY(double MaximumRate READ maximumRate)
+    Q_PROPERTY(bool CanGoNext READ canNext)
+    Q_PROPERTY(bool CanGoPrevious READ canPrevious)
+    Q_PROPERTY(bool CanPlay READ canPlay)
+    Q_PROPERTY(bool CanPause READ canPause)
+    Q_PROPERTY(bool CanSeek READ canSeek)
+    Q_PROPERTY(bool CanControl READ canControl)
+
 
 public:
     SAPMPris(QObject* parent = nullptr);
     ~SAPMPris();
 
-signals:
-    void canQuitChanged();
-    void canRaiseChanged();
-    void hasTrackListChanged();
-    void identityChanged();
-    void supportedUriSchemesChanged();
-    void supportedMimeTypesChanged();
-
+    // Root mpris methods.
 public slots:
     void Raise();
     void Quit();
 
+    // Player mpris methods.
+    void Next();
+    void Previous();
+    void Pause();
+    void PlayPause();
+    void Stop();
+    void Play();
+    void Seek(long long offset);
+    void SetPosition(const QDBusObjectPath& id, long long position);
+    void OpenUri(const QString& uri);
+
+public:
+
+    // Player properties.
+    enum class PlaybackStatus
+    {
+        PLAYING,
+        PAUSED,
+        STOPPED
+    };
+
+    struct MetaData
+    {
+        QString trackID;
+
+        QVariantMap toVariantMap() const;
+    };
+
 private:
+    // Root properties.
     bool canQuit() const;
     bool canRaise() const;
     bool hasTrackList() const;
     QString identity() const;
     QStringList supportedUriSchemes() const;
     QStringList supportedMimeTypes() const;
+
+    // Player properties.
+    QString playbackStatus() const;
+    void setPlaybackStatus(PlaybackStatus status);
+
+    double rate() const;
+    void setRate(double rate);
+
+    QVariantMap metadata() const;
+    void setMetadata(const MetaData& data);
+
+    double volume() const;
+    void setVolume(double volume);
+
+    long long position() const;
+    void setPosition(long long position);
+
+    double minimumRate() const;
+    double maximumRate() const;
+
+    bool canNext() const;
+    bool canPrevious() const;
+    bool canPlay() const;
+    bool canPause() const;
+    bool canSeek() const;
+    bool canControl() const;
+
+
+    // Helper methods.
+    static QString playbackStatusEnumToString(PlaybackStatus status);
+
+    // Make the class SystemMediaControl a friend class
+    friend class SystemMediaControl;
+
+    // Members variables.
+    QString m_playbackStatus;
+    QVariantMap m_metadata;
+    long long m_position;
+
+    // Static variables
+    static const char* _serviceName;
+    static const char* _mprisObjectPath;
+    static const char* _freeDesktopPath;
 };
 
 #endif // SIMPLEAUDIOPLAYER_SAPMPRIS_H_
--- src/dbus/SAPMPris.cpp
+++ src/dbus/SAPMPris.cpp
@@ -1,20 +1,28 @@
 #include "dbus/SAPMPris.h"
 #include <QDebug>
 #include <QtDBus/qdbusconnection.h>
+#include <qdbusextratypes.h>
+
+const char* SAPMPris::_serviceName = "org.mpris.MediaPlayer2.simpleaudioplayer";
+const char* SAPMPris::_mprisObjectPath = "/org/mpris/MediaPlayer2";
+const char* SAPMPris::_freeDesktopPath = "org.freedesktop.DBus.Properties";
 
 SAPMPris::SAPMPris(QObject* parent) :
     QObject(parent)
 {
-    QDBusConnection::sessionBus().registerService("org.mpris.MediaPlayer2.simpleaudioplayer");
+    QDBusConnection::sessionBus().registerService(_serviceName);
     QDBusConnection::sessionBus().registerObject(
-        "/org/mpris/MediaPlayer2", 
-        this, 
+        _mprisObjectPath, 
+        this,
         QDBusConnection::ExportAllSlots | QDBusConnection::ExportAllProperties);
 }
 
 SAPMPris::~SAPMPris()
 {}
 
+/*
+Root MRPIS
+*/
 void SAPMPris::Raise()
 {
     qDebug() << "SAPMPris::Raise()";
@@ -59,4 +67,182 @@ QStringList SAPMPris::supportedMimeTypes() const
 {
     qDebug() << "SAPMPRIS::SupportedMimeTypes()";
     return {};
+}
+
+/*
+Player MPRIS
+*/
+void SAPMPris::Next()
+{
+    qDebug() << "SAPMPris::Next()";
+}
+
+void SAPMPris::Previous()
+{
+    qDebug() << "SAPMPris::Previous()";
+}
+
+void SAPMPris::Pause()
+{
+    qDebug() << "SAPMPris::Pause()";
+}
+
+void SAPMPris::PlayPause()
+{
+    qDebug() << "SAPMPris::PlayPause()";
+}
+
+void SAPMPris::Stop()
+{
+    qDebug() << "SAPMPris::Stop()";
+}
+
+void SAPMPris::Play()
+{
+    qDebug() << "SAPMPris::Play()";
+}
+
+void SAPMPris::Seek(long long offset)
+{
+    qDebug() << "SAPMPris::Seek()";
+}
+
+void SAPMPris::SetPosition(const QDBusObjectPath &id, long long position)
+{
+    qDebug() << "SAPMPris::SetPosition(id, position)";
+}
+
+void SAPMPris::OpenUri(const QString& uri)
+{
+    qDebug() << "SAPMPris::OpenUri()";
+}
+
+QString SAPMPris::playbackStatus() const
+{
+    qDebug() << "SAPMPris::playbackStatus()";
+
+    return m_playbackStatus;
+}
+
+void SAPMPris::setPlaybackStatus(PlaybackStatus status)
+{
+    // Updating the playback status.
+    m_playbackStatus = playbackStatusEnumToString(status);
+}
+
+double SAPMPris::rate() const
+{
+    qDebug() << "SAPMPris::rate()";
+    return 1.0;
+}
+
+void SAPMPris::setRate(double rate)
+{
+    qDebug() << "SAPMPris::setRate()";
+}
+
+QVariantMap SAPMPris::metadata() const
+{
+    qDebug() << "SAPMPris::metadata()";
+    return m_metadata;
+}
+
+void SAPMPris::setMetadata(const MetaData& data)
+{
+    qDebug() << "SAPMPris::setMetadata()";
+    m_metadata = data.toVariantMap();
+}
+
+double SAPMPris::volume() const
+{
+    qDebug() << "SAPMPris::volume()";
+    return 1.0;
+}
+
+void SAPMPris::setVolume(double volume)
+{
+    qDebug() << "SAPMPris::setVolume()";
+}
+
+long long SAPMPris::position() const
+{
+    qDebug() << "SAPMPris::position()";
+    return m_position;
+}
+
+void SAPMPris::setPosition(long long position)
+{
+    m_position = position * 1000; // Convert millisecond value to microsecond.
+}
+
+double SAPMPris::minimumRate() const
+{
+    qDebug() << "SAPMPris::minimumRate()";
+    return 1.0;
+}
+
+double SAPMPris::maximumRate() const
+{
+    qDebug() << "SAPMPris::maximumRate()";
+    return 1.0;
+}
+
+bool SAPMPris::canNext() const
+{
+    qDebug() << "SAPMPris::canNext()";
+    return false;
+}
+
+bool SAPMPris::canPrevious() const
+{
+    qDebug() << "SAPMPris::canPrevious()";
+    return false;
+}
+
+bool SAPMPris::canPlay() const
+{
+    qDebug() << "SAPMPris::canPlay()";
+    return true;
+}
+
+bool SAPMPris::canPause() const
+{
+    qDebug() << "SAPMPris::canPause()";
+    return true;
+}
+
+bool SAPMPris::canSeek() const
+{
+    qDebug() << "SAPMPris::canSeek()";
+    return false;
+}
+
+bool SAPMPris::canControl() const
+{
+    qDebug() << "SAPMPris::canControl()";
+    return true;
+}
+
+/*
+Helper methods
+*/
+QString SAPMPris::playbackStatusEnumToString(PlaybackStatus status)
+{
+    // Converting the enum class PlaybackStatus to string for dbus.
+    if (status == PlaybackStatus::PLAYING)
+        return "Playing";
+    else if (status == PlaybackStatus::PAUSED)
+        return "Paused";
+    else
+        return "Stopped";
+}
+
+QVariantMap SAPMPris::MetaData::toVariantMap() const
+{
+    // Convert MetaData to valid dbus values.
+    QVariantMap map;
+    map["mpris:trackid"] = QDBusObjectPath(QString("/simpleaudioplayer/%1").arg(trackID));
+    map["xesam:title"] = "Hello There";
+    map["xesam:album"] = "My Super Album";
+    return map;
 }
