5797270005a4895486fdec3ff44db38aa3775429
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-02-08 18:26:01 +0100

    remove dummy items from playlist

--- src/PlaylistModel.cpp
+++ src/PlaylistModel.cpp
@@ -8,27 +8,7 @@
 
 PlaylistModel::PlaylistModel(QObject* parent) :
     SelectionModel(parent)
-{
-    QVariantList fakeTracksList = QVariantList();
-
-    Track track1 = {
-        "dummyFilePath",
-        "Track 1",
-        "Artist 1, Artist 2"
-    };
-    fakeTracksList.append(QVariant::fromValue(track1));
-
-    track1.name = "Track 2";
-    fakeTracksList.append(QVariant::fromValue(track1));
-
-    track1.name = "Track 3";
-    fakeTracksList.append(QVariant::fromValue(track1));
-
-    track1.name = "Track 4";
-    fakeTracksList.append(QVariant::fromValue(track1));
-
-    setItemList(fakeTracksList);
-}
+{}
 
 PlaylistModel::~PlaylistModel()
 {}
