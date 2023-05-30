6e403c3f1d679e8b1dd1ea685bc44be060049a5f
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-05-30 22:19:03 +0200

    if not already set, fetch best available device

--- include/spotify/SpotifyPlayer.h
+++ include/spotify/SpotifyPlayer.h
@@ -1,7 +1,9 @@
 #ifndef SIMPLEAUDIOPLAYER_SPOTIFYPLAYER_H_
 #define SIMPLEAUDIOPLAYER_SPOTIFYPLAYER_H_
 
+#include "EventCallback.h"
 #include "SpotifyAuthorizationPKCE.h"
+#include <functional>
 #include <qobject.h>
 #include <qnetworkrequest.h>
 #include <qtmetamacros.h>
@@ -23,7 +25,14 @@ signals:
 private:
     void handlePlayResponse(QNetworkReply* reply);
 
+    void fetchAvailableDevices();
+    void handleAvailableDeviceResponse(QNetworkReply* reply);
+
+    EventCallback m_eventCallback;
+
     SpotifyAuthorizationPKCE* m_spotifyAccess;
+    
+    QString m_deviceID;
 };
 
 #endif // SIMPLEAUDIOPLAYER_SPOTIFYPLAYER_H_
--- src/spotify/SpotifyPlayer.cpp
+++ src/spotify/SpotifyPlayer.cpp
@@ -1,9 +1,12 @@
 #include "spotify/SpotifyPlayer.h"
 #include <QVariantMap>
+#include <functional>
 #include <qjsondocument.h>
 #include <qjsonobject.h>
+#include <qjsonarray.h>
 #include <qnetworkrequest.h>
 #include <qnetworkreply.h>
+#include <qurlquery.h>
 
 SpotifyPlayer::SpotifyPlayer(SpotifyAuthorizationPKCE* spotifyAccess, QObject* parent) :
     QObject(parent),
@@ -15,6 +18,16 @@ SpotifyPlayer::~SpotifyPlayer()
 
 void SpotifyPlayer::play(const QVariantMap& playArguments)
 {
+    if (m_deviceID.isEmpty())
+    {
+        m_eventCallback.setCallback([this, playArguments]() {
+            this->play(playArguments);
+        });
+
+        fetchAvailableDevices();
+        return;
+    }
+
     if (!m_spotifyAccess || !m_spotifyAccess->isAuthenticated() ||
         !playArguments.contains("contex") ||
         !playArguments.contains("offset"))
@@ -34,10 +47,11 @@ void SpotifyPlayer::play(const QVariantMap& playArguments)
 
     const QString contexUri = vContexUri.toString();
     const int offset = vOffset.toInt();
-    qDebug() << "contex_uri: " << contexUri;
-    qDebug() << "offset: " << offset;
 
-    QNetworkRequest request(QUrl("https://api.spotify.com/v1/me/player/play"));
+    QUrlQuery urlQuery;
+    urlQuery.addQueryItem("device_id", m_deviceID);
+
+    QNetworkRequest request(QUrl("https://api.spotify.com/v1/me/player/play?" + urlQuery.query(QUrl::FullyEncoded)));
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
     QVariantMap jsonOffset;
@@ -49,7 +63,6 @@ void SpotifyPlayer::play(const QVariantMap& playArguments)
 
     const QByteArray bodyData =
         QJsonDocument::fromVariant(jsonBody).toJson(QJsonDocument::Compact);
-    qDebug() << "jsonBodyData: " << bodyData;
 
     QNetworkReply* reply = 
         m_spotifyAccess->put(request, bodyData);
@@ -94,4 +107,124 @@ void SpotifyPlayer::handlePlayResponse(QNetworkReply* reply)
     }
 
     emit isPlaying();
+}
+
+void SpotifyPlayer::fetchAvailableDevices()
+{
+    if (!m_spotifyAccess || !m_spotifyAccess->isAuthenticated())
+    {
+        return;
+    }
+
+    m_deviceID = QString();
+
+    QNetworkRequest request(QUrl("https://api.spotify.com/v1/me/player/devices"));
+    QNetworkReply* reply = m_spotifyAccess->get(request);
+    if (!reply) return;
+    connect(reply, &QNetworkReply::finished, [this, reply]() {
+        this->handleAvailableDeviceResponse(reply);
+    });
+}
+
+QString findMostAccurateDevice(const QJsonArray& devicesArray)
+{
+    if (devicesArray.isEmpty()) return QString();
+
+    QString activeDeviceID;
+    QString nonActiveDeviceID;
+
+    for (const QJsonValue& deviceValue : devicesArray)
+    {
+        if (deviceValue.isNull() || !deviceValue.isObject()) continue;
+
+        const QJsonObject deviceObject = deviceValue.toObject();
+
+        if (deviceObject.isEmpty() || !deviceObject.contains("id") ||
+            !deviceObject.contains("is_active") || !deviceObject.contains("is_restricted"))
+        {
+            continue;
+        }
+
+        const QString id = deviceObject.value("id").toString();
+        const bool isActive = deviceObject.value("is_active").toBool();
+        const bool isRestricted = deviceObject.value("is_restricted").toBool();
+
+        if (id.isEmpty() || isRestricted) continue;
+
+        if (isActive && activeDeviceID.isEmpty())
+        {
+            activeDeviceID = id;
+        }
+        else if (nonActiveDeviceID.isEmpty())
+        {
+            nonActiveDeviceID = id;
+        }
+
+        if (!activeDeviceID.isEmpty() && !nonActiveDeviceID.isEmpty()) break;
+    }
+
+    return !activeDeviceID.isEmpty() ? activeDeviceID :
+        !nonActiveDeviceID.isEmpty() ? nonActiveDeviceID : QString();
+}
+
+void SpotifyPlayer::handleAvailableDeviceResponse(QNetworkReply* reply)
+{
+    if (!reply) 
+    {
+        return;
+    }
+
+    const QByteArray responseData = reply->readAll();
+    reply->deleteLater();
+
+    if (responseData.isEmpty())
+    {
+        return;
+    }
+
+    QJsonParseError error;
+    const QJsonDocument jsonDocument = QJsonDocument::fromJson(responseData, &error);
+
+    if (error.error != QJsonParseError::NoError)
+    {
+        qDebug() << "Failed to parse response: " << error.errorString();
+        return;
+    }
+
+    if (!jsonDocument.isObject())
+    {
+        qDebug() << "Invalid response";
+        return;
+    }
+
+    const QJsonObject rootObject = jsonDocument.object();
+
+    if (rootObject.isEmpty() || rootObject.contains("error") || !rootObject.contains("devices"))
+    {
+        qDebug() << "Invalid response or error!";
+        return;
+    }
+
+    const QJsonValue devicesListValue = rootObject.value("devices");
+
+    if (devicesListValue.isNull() || !devicesListValue.isArray())
+    {
+        qDebug() << "Invalid devices list";
+        return;
+    }
+
+    const QJsonArray devicesArray = devicesListValue.toArray();
+
+    if (devicesArray.isEmpty())
+    {
+        qDebug() << "no availables devices";
+        return;
+    }
+
+    const QString deviceID = findMostAccurateDevice(devicesArray);
+    if (deviceID.isEmpty()) return;
+
+    m_deviceID = deviceID;
+
+    m_eventCallback.call();
 }
