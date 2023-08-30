50429e900adecfd17d7779bb4fdb5ccdce97392f
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-08-30 12:27:08 +0200

    Do not include a track if it is not available in the current market

--- src/spotify/SpotifyReceivedPlaylistElement.cpp
+++ src/spotify/SpotifyReceivedPlaylistElement.cpp
@@ -225,6 +225,18 @@ SpotifyReceivedPlaylistElement::Track SpotifyReceivedPlaylistElement::parseInner
         return {};
     };
 
+    if (!trackObject.contains("is_playable")) {
+        qDebug() << "is_playable not available";
+        return callError();
+    }
+
+    const bool isPlayable = trackObject.value("is_playable").toBool(false);
+
+    if (!isPlayable) {
+        qDebug() << "this track is not playable in the current market";
+        return callError();
+    }
+
     if (!trackObject.contains("is_local")) 
     {
         qDebug() << "is_local do not exists";
